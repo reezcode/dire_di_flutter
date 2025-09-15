7952868ce5a305e788bb6b1b5302ef421acc2b30
Reez <resmaadi@gmail.com>
2025-09-15 08:21:25 +0700

    ver:2.1.0 fix:[multiple generator]

--- lib/builder.dart
+++ lib/builder.dart
@@ -4,8 +4,5 @@ import 'src/generator/builder.dart' as gen;
 
 Builder diBuilder(BuilderOptions options) => gen.diBuilder(options);
 
-Builder diAggregatingBuilder(BuilderOptions options) =>
-    gen.diAggregatingBuilder(options);
-
 Builder diMirrorsFreeBuilder(BuilderOptions options) =>
     gen.diMirrorsFreeBuilder(options);
--- lib/src/generator/builder.dart
+++ lib/src/generator/builder.dart
@@ -1,17 +1,12 @@
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'aggregating_builder.dart';
 import 'di_generator.dart';
 import 'mirrors_free_builder.dart';
 
 /// Builder for generating dependency injection code (per-file)
 Builder diBuilder(BuilderOptions options) =>
     LibraryBuilder(DiGenerator(), generatedExtension: '.dire_di.dart');
-
-/// Aggregating builder for generating consolidated dependency injection code
-Builder diAggregatingBuilder(BuilderOptions options) =>
-    const DireDiAggregatingBuilder();
 
 /// Mirrors-free aggregating builder (recommended for Flutter/mobile)
 Builder diMirrorsFreeBuilder(BuilderOptions options) =>
